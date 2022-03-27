7b073109571678b4af6002d3fd81a9392e0945f4
2ndTry <mirnijalex@gmail.com>
2022-03-27 12:13:10 +0300

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven {url = uri("https://jitpack.io")}
+    maven {
+        url = uri("https://repo1.maven.org/maven2/")
+    }
 }
 
 dependencies {
